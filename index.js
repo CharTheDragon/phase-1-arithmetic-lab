fdb59c2bf70bd0f807386c83721f99fa2af33983
Nathaniel Swan <cygnet1.999@gmail.com>
2023-04-29 22:17:34 -0400

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+let num3 = 16
+let num4 = 6
 
-// Write your code here
+
+const multiply = num1 * num2
+
+const random = Math.round(Math.random() + 1)
+
+const mod = num3 % num4
+
+const max = Math.max(1, 2, 3, 4, 20)
